be96c8643f9798997169df22972896009de5ae4a
Diego Chavez <diego.chav.mart@gmail.com>
2025-05-22 12:09:35 -0600

    Esta es la verdadera flag

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const txt = document.querySelector('#text-flag');
 
 const actionBtn = ()=>{
     //Flag para ganar un punto en el parcial
-    alert('¿Qué andas buscando?');
+    alert('Vale por un punto extra en el parcial');
 }
 
 btn.addEventListener('click' , actionBtn);
