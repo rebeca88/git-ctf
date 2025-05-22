c187abd58a7f18665c7962e9f3172a2aaf330bea
Diego Chavez <diego.chav.mart@gmail.com>
2025-05-22 12:08:37 -0600

    Aquí esta la flag

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#btn-flag');
+const txt = document.querySelector('#text-flag');
+
+const actionBtn = ()=>{
+    //Flag para ganar un punto en el parcial
+    alert('¿Qué andas buscando?');
+}
+
+btn.addEventListener('click' , actionBtn);
