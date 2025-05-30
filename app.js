28844187164554ea9a3a6b1a330ea2891d955846
MacBook Air <macbookair@MacBook-Air-de-MacBook-2.local>
2025-05-30 12:36:01 -0600

    Ahora sí se cambió el estilo

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const btn = document.querySelector('#btn-flag');
 const txt = document.querySelector('#text-flag');
 
 const actionBtn = ()=>{
-    //Flag para ganar un punto en el parcial
-    txt.innerHTML = 'Aquí no hay flag'
+    alert('Vale por un punto extra en el parcial');
 }
 
 btn.addEventListener('click' , actionBtn);
